f0cd73342cfee47c56ef73d154e923f8fbf72896
ohchanju <ohchanju3@naver.com>
2025-06-24 02:35:08 +0900

    [Feat]#1-color setting

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,3 +1,35 @@
 export const colors = {
-  // bgbeige01: "#ede2da",
+  // blue
+  blue01: "#063A74",
+  blue02: "#0b50d0",
+  blue03: "#256ef4",
+  blue04: "#d3ebfd",
+  blue05: "#e7f4fe",
+  blue06: "#d6e0eb",
+  blue07: "#ecf2fe",
+  blue08: "#eef2f7",
+
+  // gray
+  gray01: "#121212",
+  gray02: "#464c53",
+  gray03: "#6d7882",
+  gray04: "#8a949e",
+  gray05: "#cdd1d5",
+  gray06: "#f4f5f6",
+  gray07: "#fafafa",
+
+  // red
+  red01: "#bd2c0f",
+  red02: "#fdefec",
+  red03: "#fcdfd9",
+
+  // yellow
+  yellow01: "#8a5c00",
+  yellow02: "#ffe0a3",
+  yellow03: "#fff3db",
+
+  //green
+  green01: "#267337",
+  green02: "#d8eedd",
+  green03: "#eaf6ec",
 };
