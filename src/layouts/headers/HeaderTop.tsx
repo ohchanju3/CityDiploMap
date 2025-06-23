1618e490f40c42b371576e30a10ad01852a129e5
ohchanju <ohchanju3@naver.com>
2025-06-24 04:13:33 +0900

    [Feat]#3-language modal

--- src/layouts/headers/HeaderTop.tsx
+++ src/layouts/headers/HeaderTop.tsx
@@ -1,14 +1,37 @@
 import { fonts } from "@styles/fonts";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import LanguageModal from "./components/LanguageModal";
 
 const HeaderTop = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [language, setLanguage] = useState<"ko" | "en">("ko");
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem("language") as "ko" | "en" | null;
+    if (savedLang) setLanguage(savedLang);
+  }, []);
+
+  const handleSelectLanguage = (lang: "ko" | "en") => {
+    setLanguage(lang);
+    localStorage.setItem("language", lang);
+    setIsOpen(false);
+  };
+
   return (
     <HeaderTopWrapper>
-      <HeaderTopLang>
+      <HeaderTopLang onClick={() => setIsOpen(!isOpen)}>
         <HeaderTopLagIcon src="/icon/globe.svg" />
         Language
         <HeaderTopLagIcon src="/icon/arrowDown.svg" />
       </HeaderTopLang>
+
+      {isOpen && (
+        <LanguageModal
+          currentLanguage={language}
+          onSelectLanguage={handleSelectLanguage}
+        />
+      )}
     </HeaderTopWrapper>
   );
 };
@@ -17,15 +40,16 @@ const HeaderTopWrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: flex-end;
+  position: relative;
 `;
 
 const HeaderTopLang = styled.section`
   color: ${({ theme }) => theme.colors.gray01};
-  //TODO: 추후 수정 필요
   ${fonts.header20M}
   gap: 0.125rem;
   display: flex;
   cursor: pointer;
+  align-items: center;
 `;
 
 const HeaderTopLagIcon = styled.img``;
