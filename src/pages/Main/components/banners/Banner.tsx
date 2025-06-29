be4c8055cccfc035091c5a76448ab9b010ee0af0
ohchanju <ohchanju3@naver.com>
2025-06-29 20:35:58 +0900

    [Feat]#6-banner slide

--- src/pages/Main/components/banners/Banner.tsx
+++ src/pages/Main/components/banners/Banner.tsx
@@ -1,38 +1,58 @@
+import { useState } from "react";
+import * as s from "./banner_styled";
 import Button from "@components/Button";
-import styled from "styled-components";
+
+const images = [
+  "/images/main/banner1.png",
+  "/images/main/banner1.png",
+  "/images/main/banner1.png",
+];
 
 const Banner = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const total = images.length;
+  const handleDotClick = (idx: number) => {
+    setCurrentIndex(idx);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? total - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev === total - 1 ? 0 : prev + 1));
+  };
+
   return (
-    <BannerWrapper>
-      <img src="/images/main/banner1.png" />
-      <ButtonContainer>
+    <s.BannerWrapper>
+      <s.SlideImage src={images[currentIndex]} alt={`banner-${currentIndex}`} />
+
+      <s.ButtonContainer>
         <Button
           text="자세히 보기"
           img="/icons/arrowUpRight.svg"
-          //TODO: 이동 변경하기
           onClick={() => alert("교류협력현황으로 이동 필요")}
         />
-      </ButtonContainer>
-    </BannerWrapper>
+      </s.ButtonContainer>
+
+      <s.Arrow direction="left" onClick={handlePrev}>
+        <img src="/icons/arrowLeft.svg" />
+      </s.Arrow>
+      <s.Arrow direction="right" onClick={handleNext}>
+        <img src="/icons/arrowRight.svg" />
+      </s.Arrow>
+
+      <s.Pagination>
+        {images.map((_, idx) => (
+          <s.Dot
+            key={idx}
+            $active={idx === currentIndex}
+            onClick={() => handleDotClick(idx)}
+          />
+        ))}
+      </s.Pagination>
+    </s.BannerWrapper>
   );
 };
-
-const BannerWrapper = styled.div`
-  display: flex;
-  position: relative;
-  width: 100%;
-
-  img {
-    width: 100%;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  position: absolute;
-  bottom: 23%;
-  left: 20%;
-  transform: translate(-50%, 0);
-  z-index: 2;
-`;
 
 export default Banner;
