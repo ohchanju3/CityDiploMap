9329368e4f4222f3e50fb2d832a725fe3eb227d9
ohchanju <ohchanju3@naver.com>
2025-06-29 19:19:24 +0900

    [Feat]#6-gnb 수정

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -3,13 +3,9 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const gnbItem = [
-  { label: "도시 외교 현황", onClick: "/" },
-  { label: "국별 동향 분석", onClick: "/" },
+  { label: "교류 협력 현황", onClick: "/" },
   { label: "교류 전략 추천", onClick: "/" },
-  { label: "환경외교 허브", onClick: "/" },
-  { label: "성과 뷰어", onClick: "/" },
-  { label: "사례 데이터 뱅크", onClick: "/" },
-  { label: "시민 외교 스테이션", onClick: "/" },
+  { label: "시민 외교 참여", onClick: "/" },
 ];
 
 const Gnb = () => {
