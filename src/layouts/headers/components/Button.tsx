91c45ef7a8498a91e572d03fc5d74e7484efb0ef
ohchanju <ohchanju3@naver.com>
2025-06-29 13:56:45 +0900

    [Feat]#5-버튼 활성화/비활성화 분기

--- src/layouts/headers/components/Button.tsx
+++ src/layouts/headers/components/Button.tsx
@@ -5,25 +5,29 @@ interface ButtonProps {
   onClick: () => void;
   text: string;
   img?: string;
+  disabled?: boolean;
 }
 
-const Button = ({ onClick, text, img }: ButtonProps) => {
+const Button = ({ onClick, text, img, disabled = false }: ButtonProps) => {
   return (
-    <ButtonWrapper type="button" onClick={onClick}>
+    <ButtonWrapper type="button" onClick={onClick} disabled={disabled}>
       <p>{text}</p>
       {img && <img src={img} />}
     </ButtonWrapper>
   );
 };
 
-const ButtonWrapper = styled.button`
+const ButtonWrapper = styled.button<{ disabled: boolean }>`
   display: flex;
   width: fit-content;
   height: 52px;
   padding: 14px 27px;
   border-radius: 30px;
-  background-color: ${({ theme }) => theme.colors.blue01};
-  color: ${({ theme }) => theme.colors.gray07};
+  background-color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.gray05 : theme.colors.blue01};
+  color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.gray07 : theme.colors.gray07};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   gap: 10px;
   ${fonts.body20S}
 `;
