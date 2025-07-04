d856e45c042002f7ea8b10add9d9b3007b378ca3
ohchanju <ohchanju3@naver.com>
2025-07-05 00:49:38 +0900

    [Feat]#23-국제 교류 행사 카드 슬라이드 및 api

--- src/pages/citizen/PublicDiplomacyPage.tsx
+++ src/pages/citizen/PublicDiplomacyPage.tsx
@@ -1,7 +1,4 @@
-import Button from "@components/Button";
-import MainTitle from "@components/MainTitle";
-import TabSelector from "@components/TabSelector";
-import { useState } from "react";
+import CitizenEvents from "./components/CitizenEvents";
 
 const CITY_LIST = [
   "전체",
@@ -13,22 +10,10 @@ const CITY_LIST = [
 ];
 
 const PublicDiplomacyPage = () => {
-  const [selectedCity, setSelectedCity] = useState("전체");
-
   return (
     <>
       <img src="/images/main/banner3.svg" />
-      <MainTitle
-        title="공공외교 프로그램과 국제교류 행사에 참여해보세요!"
-        subtitle="외교부 및 관계기관과 지자체에서 주최하는 프로그램 정보를 확인할 수 있어요."
-        marginTop="6.25rem"
-      />
-      <TabSelector
-        labelList={CITY_LIST}
-        selectedLabel={selectedCity}
-        onSelect={setSelectedCity}
-        marginBottom="2.5rem"
-      />
+      <CitizenEvents labelList={CITY_LIST} />
     </>
   );
 };
